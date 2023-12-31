0cdb885bbf6a3d5c0d66927bbc0bcd290d122ed5
burley85 <burley.85@osu.edu>
2023-12-31 04:58:21 -0500

    Finished decoding of jump, store, and load instructions

--- include/decoding.h
+++ include/decoding.h
@@ -1,6 +1,6 @@
 #ifndef DECODING_H
 #define DECODING_H
 
-char* decode_instruction(unsigned char* encoded_instruction);
+char* decode_instruction(unsigned char* encoded_instruction, int* encoding_length);
 
 #endif
--- src/decoding.c
+++ src/decoding.c
@@ -28,17 +28,22 @@ char* decode_register(unsigned char register_encoding){
     }
 }
 
+char* ull_to_str(unsigned long long ll){
+    char* str = malloc(21);
+    memset(str, 0, 21);
+    sprintf(str, "%llu", ll);
+    return str;
+}
+
 char* decode_literal(unsigned char* encoding, int encoding_bytes){
     unsigned long long literal = 0;
     memcpy(&literal, encoding, encoding_bytes);
 
-    char* literal_string = malloc(21);
-    memset(literal_string, 0, 21);
-    sprintf(literal_string, "%llu", literal);
-    return literal_string;
+    return ull_to_str(literal);
 }
 
-char* decode_arithmetic_instruction(unsigned char* encoded_instruction){
+
+char* decode_arithmetic_instruction(unsigned char* encoded_instruction, int* encoding_length){
     char operator;
     switch(first_n_bits(encoded_instruction[0], 4)){
         case ADD_ENCODING: operator = '+'; break;
@@ -55,12 +60,17 @@ char* decode_arithmetic_instruction(unsigned char* encoded_instruction){
 
     if(first_n_bits(encoded_instruction[1], 4) == 0){
         operand2 = decode_register(last_n_bits(encoded_instruction[1], 4));
+        *encoding_length = 2;
     }
     else{
         operand2_is_register = false;
         char operand2Length = first_n_bits(encoded_instruction[1], 4);
         if(operand2Length > 8) operand2Length = 8;
-        operand2 = decode_literal(encoded_instruction + 2, operand2Length);
+        
+        unsigned long long operand2Value = 0;
+        memcpy(&operand2Value, encoded_instruction + 2, operand2Length);
+        operand2 = ull_to_str(operand2Value);
+        *encoding_length = 2 + operand2Length;
     }
 
     char* instruction = malloc(strlen(operand1) + strlen(operand2) + 4);
@@ -71,21 +81,116 @@ char* decode_arithmetic_instruction(unsigned char* encoded_instruction){
     return instruction;
 }
 
-char* decode_load_instruction(unsigned char* encoded_instruction){
-    
+char* decode_load_instruction(unsigned char* encoded_instruction, int* encoding_length){
+    char* operand1 = decode_register(last_n_bits(encoded_instruction[0], 4));
+
+    char* operand2;
+    bool operand2_is_register = true;
+
+    if(first_n_bits(encoded_instruction[1], 4) == 0){
+        operand2 = decode_register(last_n_bits(encoded_instruction[1], 4));
+        *encoding_length = 2;
+    }
+    else{
+        operand2_is_register = false;
+        char operand2Length = first_n_bits(encoded_instruction[1], 4);
+        if(operand2Length > 8) operand2Length = 8;
+        operand2 = decode_literal(encoded_instruction + 2, operand2Length);
+        *encoding_length = 2 + operand2Length;
+    }
+
+    char* instruction = malloc(strlen(operand1) + strlen(operand2) + 6);
+    sprintf(instruction, "%s = (%s)", operand1, operand2);
+
+    if(!operand2_is_register) free(operand2);
+
+    return instruction;}
+
+char* decode_store_instruction(unsigned char* encoded_instruction, int* encoding_length){
+    char* operand1;
+    bool operand1_is_register = !check_bit(encoded_instruction[0], 4);
+    char* operand2;
+    bool operand2_is_register = !check_bit(encoded_instruction[0], 5);
+
+    if(operand1_is_register && operand2_is_register){
+        operand1 = decode_register(first_n_bits(encoded_instruction[1], 4));
+        operand2 = decode_register(last_n_bits(encoded_instruction[1], 4));
+        *encoding_length = 2;
+    }
+    else{
+        char operand1Length = 0;
+        if(!operand1_is_register){
+            memcpy_bits(&operand1Length, 5, encoded_instruction, 6, 3);
+            operand1Length++;
+            unsigned long long operand1Value = 0;
+            memcpy_bits(&operand1Value, 0, encoded_instruction + 1, 4, operand1Length * 8);
+            operand1 = ull_to_str(operand1Value);
+        }
+        else{
+            operand1 = decode_register(last_n_bits(encoded_instruction[1], 4));
+        }
+        char operand2Length = 0;
+        if(!operand2_is_register){
+            memcpy_bits(&operand2Length, 5, encoded_instruction + 1, 1, 3);
+            operand2Length++;
+            unsigned long long operand2Value = 0;
+            if(operand1_is_register) memcpy(&operand2Value, encoded_instruction + 2, operand2Length);
+            else memcpy_bits(&operand2Value, 0, encoded_instruction + 1 + operand1Length, 4, operand2Length * 8);
+            operand2 = ull_to_str(operand2Value);
+        }
+        else{
+            operand2 = decode_register(last_n_bits(encoded_instruction[1], 4));
+        }
+        *encoding_length = 2 + operand1Length + operand2Length;
+    }
+
+
+    char* instruction = malloc(strlen(operand1) + strlen(operand2) + 6);
+    sprintf(instruction, "(%s) = %s", operand1, operand2);
+
+    if(!operand1_is_register) free(operand1);
+    if(!operand2_is_register) free(operand2);
+
 }
 
-char* decode_store_instruction(unsigned char* encoded_instruction){
+char* decode_jump_instruction(unsigned char* encoded_instruction, int* encoding_length){
+    char* jump_type = "";
+    switch(first_n_bits(encoded_instruction[0], 4)){
+        case JMP_ENCODING: jump_type = "jmp"; break;
+        case JO_ENCODING: jump_type = "jo"; break;
+        case JNO_ENCODING: jump_type = "jno"; break;
+        case JZ_ENCODING: jump_type = "jz"; break;
+        case JNZ_ENCODING: jump_type = "jnz"; break;
+        case JC_ENCODING: jump_type = "jc"; break;
+        case JNC_ENCODING: jump_type = "jnc"; break;
+        case JS_ENCODING: jump_type = "js"; break;
+        case JNS_ENCODING: jump_type = "jns"; break;
+    }
+
+    char* operand1;
+    char operand1Length = last_n_bits(encoded_instruction[1], 4);
+    bool operand1_is_register = false;
+    if(operand1Length == 0){
+        operand1_is_register = true;
+        *encoding_length = 2;
+        operand1 = decode_register(first_n_bits(encoded_instruction[1], 4));
+    }
+    else{
+        *encoding_length = 1 + operand1Length;
+        operand1 = decode_literal(encoded_instruction + 2, operand1Length);
+    }
+
+    char* instruction = malloc(strlen(jump_type) + strlen(operand1) + 2);
+    sprintf(instruction, "%s %s", jump_type, operand1);
     
+    if(!operand1_is_register) free(operand1);
+
+    return instruction;
 }
 
-char* decode_jump_instruction(unsigned char* encoded_instruction){
-    
-}
-
-char* decode_instruction(unsigned char* encoded_instruction){
+char* decode_instruction(unsigned char* encoded_instruction, int* encoding_length){
     if(first_n_bits(encoded_instruction[0], 5) == KERNEL_COMMAND_ENCODING){
-
+        return "";
     }
 
     else{
@@ -96,10 +201,10 @@ char* decode_instruction(unsigned char* encoded_instruction){
             case MUL_ENCODING:
             case DIV_ENCODING:
             case EQUAL_ENCODING:
-                return decode_arithmetic_instruction(encoded_instruction);
-            case LOAD_ENCODING: return decode_load_instruction(encoded_instruction);
-            case STORE_ENCODING: return decode_store_instruction(encoded_instruction);
-            default: return decode_jump_instruction(encoded_instruction);
+                return decode_arithmetic_instruction(encoded_instruction, encoding_length);
+            case LOAD_ENCODING: return decode_load_instruction(encoded_instruction, encoding_length);
+            case STORE_ENCODING: return decode_store_instruction(encoded_instruction, encoding_length);
+            default: return decode_jump_instruction(encoded_instruction, encoding_length);
         }
     }
 
