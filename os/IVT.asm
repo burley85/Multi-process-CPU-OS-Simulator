eceefd240f9c468552f4f84e47004e90bb61c950
burley85 <burley.85@osu.edu>
2024-01-22 11:40:53 -0500

    Added function to store process's PCB

--- os/IVT.asm
+++ os/IVT.asm
@@ -89,6 +89,29 @@ clk_int_handler:
 #define PCB_TABLE_START 64
 #define PCB_SIZE 24
 
+#define PCB_TABLE_START 64
+#define PCB_SIZE 160
+#define PCB_RAX_OFFSET 0
+#define PCB_RBX_OFFSET 8
+#define PCB_RCX_OFFSET 16
+#define PCB_RDI_OFFSET 24
+#define PCB_RSI_OFFSET 32
+#define PCB_RBP_OFFSET 40
+#define PCB_RSP_OFFSET 48
+#define PCB_R8_OFFSET 56
+#define PCB_R9_OFFSET 64
+#define PCB_R10_OFFSET 72
+#define PCB_R11_OFFSET 80
+#define PCB_R12_OFFSET 88
+#define PCB_R13_OFFSET 96
+#define PCB_R14_OFFSET 104
+#define PCB_R15_OFFSET 112
+#define PCB_RIP_OFFSET 120
+#define PCB_CPU_TIME_OFFSET 128
+#define PCB_BASE_ADDR_OFFSET 136
+#define PCB_LIMIT_OFFSET 144
+#define PCB_FLAGS_OFFSET 152
+
 round_robin_scheduler:
     push r10
     push r11
@@ -133,4 +156,89 @@ round_robin_scheduler:
         pop r12
         pop r11
         pop r10
-        ret    
+        ret
+
+;Update the PCB of the current process
+;Process rip must be stored at (rsp + 24)
+;Process rsp must be stored at (rsp + 16)
+;Flag register must be stored at (rsp + 8)
+;NOTE: This function may clobber all registers
+store_PCB:
+    push rax ;Preserve rax
+
+    ;Get process's PCB address
+    rax = PCB_TABLE_START
+    rax = (rax) ;Current process's PID
+    rax * PCB_SIZE
+    rax + PCB_TABLE_START ;Address of current process's PCB
+
+    ;Store rbx
+    rax + PCB_RBX_OFFSET ;Address of PCB rbx
+    (rax) = rbx
+
+    ;Store rax
+    rbx = rax
+    rbx - PCB_RBX_OFFSET
+    rbx + PCB_RAX_OFFSET ;Address of PCB rax
+    pop rax ;Restore process's rax
+    (rbx) = rax
+
+    ;Store rcx-rbp
+    rbx - PCB_RAX_OFFSET
+    rbx + PCB_RCX_OFFSET
+    (rbx) = rcx
+    rbx + 8
+    (rbx) = rdi
+    rbx + 8
+    (rbx) = rsi
+    rbx + 8
+    (rbx) = rbp
+    rbx - PCB_RBP_OFFSET ;Reset rbx to point to start of PCB
+
+    ;Store r8-r15
+    rbx + 8
+    (rbx) = r8
+    rbx + 8
+    (rbx) = r9
+    rbx + 8
+    (rbx) = r10
+    rbx + 8
+    (rbx) = r11
+    rbx + 8
+    (rbx) = r12
+    rbx + 8
+    (rbx) = r13
+    rbx + 8
+    (rbx) = r14
+    rbx + 8
+    (rbx) = r15
+    rbx - PCB_R15_OFFSET ;Reset rbx
+
+    ;Get process's flags, rsp, and rip from stack
+    rsp + 8 ;Pointer to process's flags
+    r8 = (rsp) ;r8 = flags
+    rsp + 8 ;Pointer to process's rsp
+    r9 = (rsp) ;r9 = rsp
+    rsp + 8 ;Pointer to process's rip
+    r10 = (rsp) ;r10 = rip
+    rsp - 24
+
+    ;Store flags and rip
+    rbx + PCB_FLAGS_OFFSET
+    (rbx) = r8
+    rbx - PCB_FLAGS_OFFSET
+    rbx + PCB_RSP_OFFSET
+    (rbx) = r9
+    rbx - PCB_RSP_OFFSET
+    rbx + PCB_RIP_OFFSET
+    (rbx) = r10
+    rbx - PCB_RIP_OFFSET ;Reset rbx
+
+    ;Store mmu base/ limit
+    r8 = cr3 ;Base
+    r9 = cr4 ;Limit
+    rbx + PCB_BASE_ADDR_OFFSET
+    (rbx) = r8
+    rbx - PCB_BASE_ADDR_OFFSET
+    rbx + PCB_LIMIT_OFFSET
+    (rbx) = r9
