1e1ff69d8aed33ea9393afcfb1dcab271c292937
burley85 <burley.85@osu.edu>
2024-03-29 12:17:41 -0400

    Implemented Calls

--- compiler/nodes/Call.py
+++ compiler/nodes/Call.py
@@ -1,5 +1,6 @@
 from Compiler import Compiler, TokenType
 from nodes.ASTNode import ASTNode
+from nodes.MethodHeader import MethodHeader
 
 '''<call> ::= <identifier> "(" <argument_list> ")" | <identifier> "(" ")"
    <argument_list> ::= <id_or_literal> | <id_or_literal> "," <argument_list>'''
@@ -15,6 +16,32 @@ class Call(ASTNode):
         while compiler.currentToken().type != TokenType.RPAREN:
             self.argument_list.append(compiler.expect(idOrLiteral))
             if compiler.currentToken().type != TokenType.RPAREN: compiler.expect(TokenType.COMMA)
+        compiler.expect(TokenType.RPAREN)
         return self
     
-    def print(self, file, indent = ""): pass
+    def print(self, file, indent = ""): 
+        print(f'{indent}{self.identifier}(', file = file, end = "")
+        for argument in self.argument_list:
+            print(argument.value, file = file, end = "")
+            if argument != self.argument_list[-1]:
+                print(", ", file = file, end = "")
+        print(")", file = file, end = "")
+
+    def compile(self, compiler: Compiler, file):
+        #Check number of args
+        methodHeader = compiler.findDeclaration(self.identifier)
+        if not hasattr(methodHeader, "parameterList"):
+            compiler.genericError(f"'{self.identifier}' is not a method")
+        if len(methodHeader.parameterList) != len(self.argument_list):
+            compiler.genericError(f"Incorrect number of arguments for '{self.identifier}'")
+        #Push args onto stack
+        for (parameter, argument) in zip(methodHeader.parameterList, self.argument_list):
+            #Load arg
+            argDecl = compiler.findDeclaration(argument.value)
+            print(f"rbp {'-' if argDecl.stackOffset > 0 else '+'} {abs(argDecl.stackOffset)}", file = file)
+            print("rax = (rbp)", file = file)
+            print(f"rbp {'+' if argDecl.stackOffset > 0 else '-'} {abs(argDecl.stackOffset)}", file = file)
+            #Push arg onto stack
+            print("push rax", file = file)
+        #jump to method
+        print(f"call _{self.identifier}", file = file)
